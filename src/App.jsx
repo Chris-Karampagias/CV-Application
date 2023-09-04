3caaa87ccea578967ffddd0fdbaaebe00755589a
Chris <karampagiaschris@gmail.com>
2023-09-04 12:13:19 +0300

    Modify CvCreator

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import "./App.css";
 import GeneralInfo from "./components/general-info";
 import EducationalExperience from "./components/educational-experience";
 import PracticalExperience from "./components/practical-experience";
-import CvCreator from "./components/cv-creator";
+import CV from "./components/cv-creator";
 
 function App() {
   return (
-    <div className="app-container">
-      <CvCreator>
+    <>
+      <CV>
         <GeneralInfo />
         <EducationalExperience />
         <PracticalExperience />
-      </CvCreator>
-      <div className="cv-preview-container"></div>
-    </div>
+      </CV>
+    </>
   );
 }
 
--- src/components/cv-creator.jsx
+++ src/components/cv-creator.jsx
@@ -1,15 +1,20 @@
 /* eslint-disable react/prop-types */
 import "../styles/cv-creator.css";
+import { useState } from "react";
+import CvPreview from "./cv-preview";
 
-export default function CvCreator({ children }) {
+export default function CV({ children }) {
   return (
-    <div className="cv-creator-container">
-      <h1 className="cv-creator-name">CV Creator</h1>
-      {children}
-      <div className="buttons-container">
-        <button type="submit">Submit</button>
-        <button type="button">Edit</button>
+    <div className="cv-container">
+      <div className="cv-creator-container">
+        <h1 className="cv-creator-name">CV Creator</h1>
+        {children}
+        <div className="buttons-container">
+          <button type="submit">Submit</button>
+          <button type="button">Edit</button>
+        </div>
       </div>
+      <CvPreview />
     </div>
   );
 }
